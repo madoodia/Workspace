b0cdf9d888efe512493e65863f506b5350cde0b9
Mahmoodreza Aarabi <madoodia@gmail.com>
2020-04-02 12:15:07 -0700

    debug mode is added

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,22 @@ int main(int argc, char *argv[])
 {
   printf("Hello From main\n");
 
-#if USE_GTEST
+#if 0
   ::testing::InitGoogleTest(&argc, argv);
   int testResult = RUN_ALL_TESTS();
 #endif // USE_GTEST
+
+  int data1 = 1;
+  int data2 = 2;
+  int data3 = 3;
+  int data4 = 4;
+  int data5 = 5;
+
+  int *pData1 = &data1;
+  const int *pData2 = &data2;
+  int *const pData3 = &data3;
+  const int *const pData4 = &data4;
+  int const *pData5 = &data5;
 
   return 0;
 }
