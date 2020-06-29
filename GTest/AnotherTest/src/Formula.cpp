b42e65a969bd0c879ddacf70580ab07163aa16a5
Mahmoodreza Aarabi <madoodia@gmail.com>
2020-06-29 11:53:43 -0700

    remove temporary GTest folder and setup new TEST system

--- GTest/AnotherTest/src/Formula.cpp
+++
@@ -1,6 +0,0 @@
-#include "Formula.h"
-
-int Formula::bla(int arg1)
-{
-  return arg1 * 2;
-}
--- GTest/AnotherTest/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Formula.h"
-#include <iostream>
-
-int main()
-{
-  std::cout << "Bla: " << Formula::bla(2) << std::endl;
-  return 0;
-}
